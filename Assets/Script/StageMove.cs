da4546852a97be28e113c10f1de20dff2cf3615b
tyaool <czamy00@gmail.com>
2020-01-14 23:27:58 +0900

    ステージを動かせるようにした リセットボタン追加

---
+++ Assets/Script/StageMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class StageMove : MonoBehaviour {
+
+    [SerializeField] Slider SliderX;
+    [SerializeField] Slider SliderY;
+    [SerializeField] Slider SliderZ;
+
+    [SerializeField] Transform StageTra;
+
+    Vector3 rotate;
+
+    public void RotX()
+    {
+        rotate.x = SliderX.value;
+        StageTra.eulerAngles = rotate;
+    }
+
+    public void RotY()
+    {
+        rotate.y = SliderY.value;
+        StageTra.eulerAngles = rotate;
+    }
+
+    public void RotZ()
+    {
+        rotate.z = SliderZ.value;
+        StageTra.eulerAngles = rotate;
+    }
+
+    public void ValueReset()
+    {
+        SliderX.value = 0f;
+        SliderY.value = 0f;
+        SliderZ.value = 0f;
+    }
+}
