583b671ddc0b50f9df54c790a6bc5314b90bf855
ぷっちょ <lieutenantcommander.mystical@gmail.com>
2020-01-22 01:48:25 +0900

    説明追加

---
+++ Assets/Script/ExplainController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExplainController : MonoBehaviour {
+
+    GameObject explainpic;
+    //Touch touch;
+
+    // Use this for initialization
+    void Start () {
+        explainpic = new GameObject("ExplainPic");
+        explainpic.transform.parent = GameObject.Find("Canvas").transform;
+        explainpic.AddComponent<RectTransform>().anchoredPosition = new Vector3(0, 0, 0);
+        explainpic.GetComponent<RectTransform>().localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        explainpic.AddComponent<Image>().sprite = Resources.Load<Sprite>("Explain");
+        explainpic.GetComponent<Image>().preserveAspect = true;
+        explainpic.GetComponent<Image>().SetNativeSize();
+        explainpic.GetComponent<Image>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*Touch touch = Input.GetTouch(0);
+
+        if(touch.phase==TouchPhase.Began)
+        { explainpic.GetComponent<Image>().enabled = false; }
+        */
+        if(Input.GetMouseButtonDown(0))
+        { explainpic.GetComponent<Image>().enabled = false; }
+	}
+
+    public void ImageEnable(){
+        explainpic.GetComponent<Image>().enabled = true;
+        
+    }
+
+}
