56c54a833947b3d4af01c3760ef3ffa6f20ff93c
tyaool <czamy00@gmail.com>
2020-01-21 17:45:42 +0900

    あ

---
+++ Assets/Script/isGoal.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isGoal : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        FadeController.FadeFlag();
+    }
+}
